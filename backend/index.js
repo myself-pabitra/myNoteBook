040f76477a08619d84329d29ebaed74453966095
pabitra <pabitrapandit2001@gmail.com>
2023-05-07 19:17:01 +0530

    FIrst Commit

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+// const connectTOMongo = require("./db");
+
+// connectTOMongo();
+
+const connectToMongo = require('./db');
+connectToMongo();
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello Pabitra welcome !')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
